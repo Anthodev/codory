174329617cfbf18f398e31e298607fd2ad740aa0
Cédric Anthony <Anthodev@users.noreply.github.com>
2025-10-08 15:27:51 +0200

    :sparkles: (action): Added install_zsh_plugin_completions (#36)

---
+++ internal/actions/shells/zsh/plugins/install_zsh_plugin_completions.go
@@ -0,0 +1,24 @@
+package shells
+
+import "anthodev/codory/internal/actions"
+
+func InstallZshPluginCompletions() *actions.Action {
+	return &actions.Action{
+		ID: "install_zsh_plugin_completions",
+		Name: "Install Zsh Plugin zsh-completions",
+		Description: "Installs the zsh-completions plugin for oh-my-zsh",
+		Type: actions.ActionTypeCommand,
+		PlatformCommands: map[actions.Platform]actions.PlatformCommand{
+			actions.PlatformLinux: {
+				Command: "git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions",
+				PackageSource: actions.PackageSourceAny,
+				CheckCommand: "which git && (test -d $HOME/.oh-my-zsh || which omz) && test -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions",
+			},
+			actions.PlatformMacOS: {
+				Command: "git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions",
+				PackageSource: actions.PackageSourceAny,
+				CheckCommand: "which git && (test -d $HOME/.oh-my-zsh || which omz) && test -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions",
+			},
+		},
+	}
+}
